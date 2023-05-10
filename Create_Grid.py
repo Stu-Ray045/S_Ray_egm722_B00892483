7628510e5d0157322a541802a292fccccc8110c6
Stu-Ray045 <127782507+Stu-Ray045@users.noreply.github.com>
2023-05-10 12:00:23 +0100

    Set CRS

--- Create_Grid.py
+++ Create_Grid.py
@@ -1,3 +1,5 @@
 import geopandas as gpd # Import the geopandas module
 from shapely import geometry # Import the geometry function from the shapely module
 
+# determine grid parameters
+set_crs(EPSG:3857) # Set the project crs
